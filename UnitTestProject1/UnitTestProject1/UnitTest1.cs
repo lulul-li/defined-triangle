c250caca9f13d003e426e9f07465488bbdd27fdb
lulu.li29 <s987337613@gmail.com>
2019-05-12 15:15:25 +0800

    add is acute triangle and is obtuse triangle

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,26 +1,44 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 namespace UnitTestProject1
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void when_side_3_4_5_should_be_Right_triangle()
+        [Test]
+        public void when_side_3_4_5_should_be_right_triangle()
         {
-            var triangle = new Triangle(3, 4, 5);
+            var triangle = new Triangle(3.0, 4.0, 5.0);
             var type = triangle.GetTriangleType();
             Assert.AreEqual("Right triangle", type);
+        }
+
+        [Test]
+        public void when_side_10_12_15_should_be_acute_triangle()
+        {
+            var triangle = new Triangle(10, 12, 15);
+            var type = triangle.GetTriangleType();
+            Assert.AreEqual("Acute triangle", type);
+        }
+
+        [Test]
+        public void when_side_6_7_12_should_be_obtuse_triangle()
+        {
+            var triangle = new Triangle(6, 7, 10);
+            var type = triangle.GetTriangleType();
+            Assert.AreEqual("Obtuse triangle", type);
         }
     }
 
     public class Triangle
     {
-        private readonly int _side1;
-        private readonly int _side2;
-        private readonly int _side3;
+        private readonly double _side1;
+        private readonly double _side2;
+        private readonly double _side3;
 
-        public Triangle(int side1, int side2, int side3)
+        public Triangle(double side1, double side2, double side3)
         {
             _side1 = side1;
             _side2 = side2;
@@ -29,7 +47,36 @@ namespace UnitTestProject1
 
         public string GetTriangleType()
         {
-            return "";
+            var sides = new[] { _side1, _side2, _side3 }.OrderBy(x => x).ToArray();
+
+            if (IsRightTriangle(sides))
+            {
+                return "Right triangle";
+            }
+            else if (IsAcuteTriangle(sides))
+
+            {
+                return "Acute triangle";
+            }
+            else
+            {
+                return "Obtuse triangle";
+            }
+        }
+
+        private static bool IsAcuteTriangle(double[] sides)
+        {
+            return square(sides[0]) + square(sides[1]) > square(sides[2]);
+        }
+
+        private static bool IsRightTriangle(double[] sides)
+        {
+            return square(sides[0]) + square(sides[1]) == square(sides[2]);
+        }
+
+        private static double square(double number)
+        {
+            return Math.Pow(number, 2);
         }
     }
 }
