48aa55f6306709dcab044f1c8d571d494edf47c4
lulu.li29 <s987337613@gmail.com>
2019-05-12 14:44:49 +0800

    add test when_side_3_4_5_should_be_Right_triangle

---
+++ UnitTestProject1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aff2e7b2-a4fe-4c60-b48b-9d4de47ca31d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void when_side_3_4_5_should_be_Right_triangle()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var type = triangle.GetTriangleType();
+            Assert.AreEqual("Right triangle", type);
+        }
+    }
+
+    public class Triangle
+    {
+        private readonly int _side1;
+        private readonly int _side2;
+        private readonly int _side3;
+
+        public Triangle(int side1, int side2, int side3)
+        {
+            _side1 = side1;
+            _side2 = side2;
+            _side3 = side3;
+        }
+
+        public string GetTriangleType()
+        {
+            return "";
+        }
+    }
+}
